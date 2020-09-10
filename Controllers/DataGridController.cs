fae052fbb2b0a229bf7d22e6c8ea1cb06b0e4c68
faithckorir <faithckorir@gmail.com>
2020-09-10 09:59:29 +0300

    first commit

---
+++ Controllers/DataGridController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevExtremeMvcApp1.Controllers
+{
+    public class DataGridController : Controller
+    {
+        // GET: DataGrid
+        public ActionResult Overview()
+        {
+            return View();
+        }
+    }
+}
